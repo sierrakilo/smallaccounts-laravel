a1db8094ee4ef96e5d40193d04c45207f0ca1c99
Sharif Khan <sharif.khan@mac.com>
2020-04-26 17:05:09 +0100

    #3.3 Part 4 - Install tailwind and Vue for drop-in components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,8 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <example-component></example-component>
     </div>
 </body>
 </html>
--- resources/views/nominal_accounts/index.blade.php
+++ resources/views/nominal_accounts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
+<div class="flex flex-col p-2 bg-gray-500">
     @foreach($nominalAccounts as $nominalAccount)
     <div>{{ $nominalAccount->name }}</div>
     @endforeach
